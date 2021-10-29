cb62dd632e5577aa54ccff20fce10a204edc23ad
giulio <giulio09@marcov.it>
2021-10-29 12:23:06 +0000

    Plotter

--- tfest/tfest.py
+++ tfest/tfest.py
@@ -8,6 +8,12 @@ class TfEst:
     def __init__(self, u, y):
         self.u = u
         self.y = y
+        self.res = None
+        self.frequency = None
+        self.H = None
+        self.npoles = 0
+        self.nzeros = 0
+        self.init_value = 1
 
     def loss(x, npoles, freq, H):
         poles = x[:npoles]
@@ -15,17 +21,35 @@ class TfEst:
         risp = np.array([
             (1+sum([a*1j*s**i for i, a in enumerate(zeros)]))/(1+sum([b*1j*s**i for i, b in enumerate(poles)]))
             for s in freq])
-        return np.linalg.norm(H-risp).sum()
-        #  + np.abs(x.sum())
+        return np.linalg.norm(H-risp).sum() # + np.abs(x.sum())
 
     def frequency_response(self):
         cross_sd, frequency = csd(self.y, self.u)
         power_sd, _ = psd(self.u)
-        return cross_sd/power_sd, frequency
+        self.frequency = frequency
+        self.H = cross_sd/power_sd
+        return self.H, frequency
 
     def estimate(self, npoles, nzeros, init_value=1):
+        self.npoles = npoles
+        self.nzeros = nzeros
+        self.init_value = init_value
+
         x0 = [init_value]*(npoles+nzeros)
         H, frequency = self.frequency_response()
         pass_to_loss = lambda x: self.loss(x, npoles, frequency, H)
-        res = minimize(pass_to_loss, x0, method='nelder-mead', options={'xatol': 1e-4, 'disp': True})
-        return res
+        self.res = minimize(pass_to_loss, x0, method='nelder-mead', options={'xatol': 1e-4, 'disp': True})
+        return self.res
+    
+    def plot(self):
+        if self.res == None:
+            raise Exception("Please run the .estimate(npoles, nzeros) before plotting.")
+        zeros = self.res.x[:self.npoles]
+        poles = self.res.x[self.npoles:]
+        risp = np.array([
+            (1+sum([a*1j*s**i for i, a in enumerate(zeros)]))/(1+sum([b*1j*s**i for i, b in enumerate(poles)]))
+            for s in self.frequency])
+        plt.plot(-np.log(risp), label="estimation")
+        plt.plot(np.log(self.H), label="train data")
+        plt.legend(loc="upper right")
+        plt.show()

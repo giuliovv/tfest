0ae1068abc0cb0f783abc9e937d75add5466a470
giulio <giulio09@marcov.it>
2021-10-29 19:10:31 +0000

    Comments

--- tfest/tfest.py
+++ tfest/tfest.py
@@ -3,6 +3,7 @@ import numpy as np
 
 from matplotlib.mlab import psd, csd
 from scipy.optimize import minimize
+from scipy import signal
 
 class TfEst:
     def __init__(self, u, y):
@@ -16,6 +17,14 @@ class TfEst:
         self.init_value = 1
 
     def loss(x, npoles, freq, H):
+        """
+        x: array of zeros and poles
+        npoles: number of poles
+        freq: frequency
+        H: frequency response
+
+        return: loss
+        """
         poles = x[:npoles]
         zeros = x[npoles:]
         risp = np.array([
@@ -24,13 +33,24 @@ class TfEst:
         return np.linalg.norm(H-risp).sum() # + np.abs(x.sum())
 
     def frequency_response(self):
+        """
+        return: frequency response and frequency
+        """
         cross_sd, frequency = csd(self.y, self.u)
         power_sd, _ = psd(self.u)
         self.frequency = frequency
         self.H = cross_sd/power_sd
         return self.H, frequency
 
-    def estimate(self, npoles, nzeros, init_value=1):
+    def estimate(self, npoles, nzeros, init_value=1, options={'xatol': 1e-4, 'disp': True}):
+        """
+        npoles: number of poles
+        nzeros: number of zeros
+        init_value: initial value for optimization
+        options: options for scipy.optimize.minimize
+
+        return: scipy.optimize.minimize.OptimizeResult
+        """
         self.npoles = npoles
         self.nzeros = nzeros
         self.init_value = init_value
@@ -38,12 +58,25 @@ class TfEst:
         x0 = [init_value]*(npoles+nzeros)
         H, frequency = self.frequency_response()
         pass_to_loss = lambda x: self.loss(x, npoles, frequency, H)
-        self.res = minimize(pass_to_loss, x0, method='nelder-mead', options={'xatol': 1e-4, 'disp': True})
+        self.res = minimize(pass_to_loss, x0, method='nelder-mead', options=options)
         return self.res
+
+    def get_transfer_function(self):
+        """
+        return: transfer function
+        """
+        if self.res == None:
+            raise Exception("Please run .estimate(npoles, nzeros) before plotting.")
+        zeros = self.res.x[:self.npoles]
+        poles = self.res.x[self.npoles:]
+        return signal.TransferFunction(zeros, poles)
     
     def plot(self):
+        """
+        Plot the frequency response and the transfer function
+        """
         if self.res == None:
-            raise Exception("Please run the .estimate(npoles, nzeros) before plotting.")
+            raise Exception("Please run .estimate(npoles, nzeros) before plotting.")
         zeros = self.res.x[:self.npoles]
         poles = self.res.x[self.npoles:]
         risp = np.array([

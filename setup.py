e93a0c559eabd81e24bab2dda23e903908ec8b81
giulio <giulio09@marcov.it>
2021-10-29 12:32:24 +0000

    Pip ready

--- setup.py
+++ setup.py
@@ -1,25 +1,26 @@
 from distutils.core import setup
 setup(
-  name = 'tfest',         # How you named your package folder (MyLib)
-  packages = ['tfest'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Transfer function estimation based on frequency response.',   # Give a short description about your library
-  author = 'Giulio Vaccari',                   # Type in your name
-  author_email = 'io@giuliovaccari.it',      # Type in your E-Mail
-  url = 'https://github.com/giuliovv/tfest',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/giuliovv/tfest/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['tfest', 'frequency', 'matlab'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+  name = 'tfest',
+  packages = ['tfest'],
+  version = '0.1',
+  license='MIT',
+  description = 'Transfer function estimation based on frequency response.',
+  author = 'Giulio Vaccari',
+  author_email = 'io@giuliovaccari.it',
+  url = 'https://github.com/giuliovv/tfest',
+  download_url = 'https://github.com/giuliovv/tfest/archive/refs/tags/v0.1-alpha.tar.gz',
+  keywords = ['tfest', 'frequency', 'matlab'],
+  install_requires=[
+          'matplotlib',
+          'numpy',
+          'scipy'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
